1eada5df3e4aaac5a8dad44bb5fd46b55015d597
RandolphLIN <2933089932@qq.com>
2017-03-23 02:34:11 +0800

    modify

--- js/change.js
+++ js/change.js
@@ -2,6 +2,9 @@
 			u();
 			q();
 			z();
+			u1();
+			q1();
+			z1();
 		};
 
 		function u(){
@@ -32,5 +35,37 @@
 		},false);
 		var img3 =document.getElementById('img3');
 		img3.addEventListener('mousedown', function(event){
+		document.getElementById('img3').src="e23.jpg";
+		},false);};
+
+
+		function u1(){
+		var img1 =document.getElementById('img1');
+		img1.addEventListener('touchend', function(event){
+		document.getElementById('img1').src="e11.jpg";
+		console.log('up');
+		},false);
+		img1.addEventListener('touchstart', function(event){
+		document.getElementById('img1').src="e21.jpg";
+		console.log('down');
+		},false);};
+
+		function q1(){
+		var img2 =document.getElementById('img2');
+		img2.addEventListener('touchend', function(event){
+		document.getElementById('img2').src="e12.jpg";
+		},false);
+		var img2 =document.getElementById('img2');
+		img2.addEventListener('touchstart', function(event){
+		document.getElementById('img2').src="e22.jpg";
+		},false);};
+
+		function z1(){
+		var img3 =document.getElementById('img3');
+		img3.addEventListener('touchend', function(event){
+		document.getElementById('img3').src="e13.jpg";
+		},false);
+		var img3 =document.getElementById('img3');
+		img3.addEventListener('touchstart', function(event){
 		document.getElementById('img3').src="e23.jpg";
 		},false);};
